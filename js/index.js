6567a954d1aa571c426f074242cc410bbd4e218b
Roman <poker_76@mail.ru>
2021-10-25 03:53:14 +0300

    add inputs

--- js/index.js
+++ js/index.js
@@ -694,3 +694,90 @@ if (triggerBurger && layoutBurger) {
     burger.listen();
 }
 
+
+//вот так можно получить значение из инпута числом +inputs.map((input) => input.value).join('')
+
+const form = document.querySelector('form');
+const inputs = [...form.querySelectorAll('input')];
+const KEYBOARDS = {
+    backspace: 8,
+    arrowLeft: 37,
+    arrowRight: 39,
+};
+
+const handleInput = (e) => {
+    const input = e.target;
+    const nextInput = input.nextElementSibling;
+    if (nextInput && input.value) {
+        nextInput.focus();
+        if (nextInput.value) {
+            nextInput.select();
+        }
+    }
+};
+
+const handlePaste = (e) => {
+    e.preventDefault();
+    const paste = e.clipboardData.getData('text').replace(/[^0-9.]/g, '');
+    inputs.slice(inputs.indexOf(e.target)).forEach((input, i) => {
+        input.value = paste[i] || '';
+    });
+};
+
+const handleBackspace = (e) => {
+    const input = e.target;
+    if (input.value) {
+        input.value = '';
+        return;
+    }
+
+    if (input.previousElementSibling) {
+        input.previousElementSibling.focus();
+    }
+};
+
+const handleArrowLeft = (e) => {
+    const previousInput = e.target.previousElementSibling;
+    if (!previousInput) {
+        return;
+    }
+    previousInput.focus();
+};
+
+const handleArrowRight = (e) => {
+    const nextInput = e.target.nextElementSibling;
+    if (!nextInput) {
+        return;
+    }
+    nextInput.focus();
+};
+
+form.addEventListener('input', handleInput);
+
+inputs.forEach((input) => {
+    input.addEventListener('focus', (e) => {
+        setTimeout(() => {
+            e.target.select();
+        }, 0);
+    });
+
+    input.addEventListener('keydown', (e) => {
+        switch(e.keyCode) {
+            case KEYBOARDS.backspace:
+                handleBackspace(e);
+                break;
+            case KEYBOARDS.arrowLeft:
+                handleArrowLeft(e);
+                break;
+            case KEYBOARDS.arrowRight:
+                handleArrowRight(e);
+                break;
+            default:
+        }
+    });
+
+    input.addEventListener('paste', handlePaste);
+    input.addEventListener('input', () => {
+        input.value = input.value.replace(/[^0-9.]/g, '');
+    });
+})
